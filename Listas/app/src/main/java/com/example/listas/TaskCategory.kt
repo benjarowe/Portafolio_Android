6ddba1a4c9b00f10822ae6a6a1a4034eba01cea3
benjarowe <benjarowe90@gmail.com>
2024-04-08 16:32:02 +0200

    Eliminando carpeta Listas/app/src/main

--- Listas/app/src/main/java/com/example/listas/TaskCategory.kt
+++
@@ -1,7 +0,0 @@
-package com.example.listas.Categorias
-
-sealed class TaskCategory (var isSelected:Boolean = true){
-    object Personal : TaskCategory()
-    object Business : TaskCategory()
-    object Other : TaskCategory()
-}
